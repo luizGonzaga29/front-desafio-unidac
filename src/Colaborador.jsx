0229dce528201ea85a8e9d84d759f16d67fb9323
email <luizgonzaga29>
2022-12-06 12:32:28 -0300

    Mudando backend para railway 1

--- src/Colaborador.jsx
+++ src/Colaborador.jsx
@@ -1,7 +1,7 @@
 
 import axios from "axios";
 import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 
